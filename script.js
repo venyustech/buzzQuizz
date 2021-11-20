81ca6f748275fafca17f0abb05ba9b1815e4a5c5
Gardenia Georgia Barbosa De Siqueira <venyustech@gmail.com>
2021-11-20 08:30:33 -0300

    obtendo unico quizz do servidor e imprimindo-o na tela

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ let quizzesUsuarioArmazenados = 0; //variável global onde será alterado o valo
 carregarQuizzes();
 
 function carregarQuizzes() {
-  console.log("tá indo aqui");
-  const quizzes = axios.get(
+  //console.log("tá indo aqui");
+  let quizzes = axios.get(
     "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
   );
   quizzes.then(exibicaoDeQuizz);
@@ -25,7 +25,7 @@ function carregarQuizzes() {
 
 function verificacaoDeErro(erro) {
   alert("deu ruim");
-  console.log(erro);
+  //console.log(erro);
 }
 
 function exibicaoDeQuizz(resposta) {
@@ -38,13 +38,13 @@ function exibicaoDeQuizz(resposta) {
 
   for (let ii = 0; ii < resposta.data.length; ii++) {
     if (nomes.includes(resposta.data[ii].title)) {
-      console.log("já tem um quizz com esse nome");
+      //console.log("já tem um quizz com esse nome");
       //verificar mais uma variável para ver se é mesmo o mesmo quizz
       imprimir = false;
     } else {
       nomes.push(resposta.data[ii].title);
       capas.push(resposta.data[ii].image);
-      console.log("ok, é um quizz novo");
+      //console.log("ok, é um quizz novo");
       imprimir = true;
     }
 
@@ -63,7 +63,7 @@ function exibicaoDeQuizz(resposta) {
     }
   }
   //fim criacao quizz
-  console.log(nomes);
+  //console.log(nomes);
   nomes = [];
   capas = [];
 }
@@ -79,11 +79,11 @@ function buscarQuizzSelecionado(id) {
 }
 
 function criarQuizzSelecionado(resposta) {
-  console.log("selecionou um quizz");
+  //console.log("selecionou um quizz");
   // se o quizz existir, e ele for selecionado, a tela1 será escondida para abrir a "tela2" com o quizz
   let tela_anterior = document.querySelector(".tela1");
   tela_anterior.classList.toggle("hide");
-  console.log(tela_anterior);
+  //console.log(tela_anterior);
   let tela_atual = document.querySelector(".embrulho-quizz-tela2");
   tela_atual.classList.toggle("hide");
   //criar o quiz com os itens da resposta
@@ -92,34 +92,67 @@ function criarQuizzSelecionado(resposta) {
 function tela1tela3(resposta) {
   let tela_anterior = document.querySelector(".tela1");
   tela_anterior.classList.toggle("hide");
-  console.log(tela_anterior);
+  //console.log(tela_anterior);
   let tela_atual = document.querySelector(".tela3-1");
   tela_atual.classList.toggle("hide");
-  console.log("ok");
+  //console.log("ok");
 }
 
-/*****************************************
+/******************************************
  *         COnfigurações tela 2:          *
  ******************************************/
-
-document.getElementById("c1").style.backgroundColor = "#434CA0";
-document.getElementById("c2").style.backgroundColor = "#a0438d";
-function abreQuizz() {
-  document.getElementById("titulo1").style.backgroundImage =
-    "url('assets/tela2/Rectangle 42.png')";
-  document.getElementById("pergunta1").style.backgroundColor = "#434CA0";
-  document.getElementById("pergunta2").style.backgroundColor = "red";
-
-  const tituloDaPaginaLoading = document.querySelector(".quizz-titulo");
-  tituloDaPaginaLoading.innerHTML += `<p>O quão PotterHead é você?</p>`;
-
-  let tituloDaPergunta = document.querySelector("#pergunta1");
-  tituloDaPergunta.innerHTML += `<p>Em qual animal Olho-Tonto Moody transfigurou Malfoy?</p>`;
-
-  tituloDaPergunta = document.querySelector("#pergunta2");
-  tituloDaPergunta.innerHTML += `<p>Qual dos objetos abaixo NÃO é uma horcrux?</p>`;
+let id = 0;
+function mostraQuizz(idt) {
+  id = idt;
+  const promisse = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`);
+  promisse.then(quizzLoading);
+  promisse.catch(loadingQuizzError);
 }
-abreQuizz();
+mostraQuizz(1);  /*********************************************************
+                  *     mostraQuizz(idt) precisa ser chamado na função    *
+                  *     que abre o quizz (recebendo o id como parametro)  *
+                  *********************************************************/
+function loadingQuizzError(error) {
+  alert("error#1 - Não foi possivel carregar esse quizz");
+}
+function quizzLoading(answer) {
+  const quizzPerguntar = answer.data;
+  //console.log(quizzPerguntar);
+  //console.log(quizzPerguntar.questions.length);
+  const tituloDaPaginaLoading = document.querySelector(".embrulho-quizz-tela2");
+  tituloDaPaginaLoading.innerHTML = `<div class="quizz-titulo" id="titulo${id}">
+<p>${quizzPerguntar.title}</p>
+</div>`;
+  document.querySelector(".quizz-titulo").style.backgroundImage = `url('${quizzPerguntar.image}')`;
+
+  for (let i = 0; i < quizzPerguntar.questions.length; i++) {
+    tituloDaPaginaLoading.innerHTML += `<div class="pergunta-embrulho">
+    <div class="pergunta-caixa">
+      <div class="pergunta-titulo" id="c${i + 1}"><p>${quizzPerguntar.questions[i].title}</p>
+    </div>
+    <div class="cards-embrulho">
+      <div class="card">
+        <img class="card-image" src="${quizzPerguntar.questions[i].answers[0].image}" height = "175.2px"  width="329.91px"/>
+        <div class="card-titulo"><p>${quizzPerguntar.questions[i].answers[0].text}</p></div>
+      </div>
+      <div class="card">
+        <img class="card-image" src="${quizzPerguntar.questions[i].answers[1].image}" height = "175.2px"  width="329.91px"/>
+        <div class="card-titulo"><p>${quizzPerguntar.questions[i].answers[1].text}</p></div>
+      </div>
+      <div class="card">
+        <img class="card-image" src="${quizzPerguntar.questions[i].answers[2].image}"  height = "175.2px"  width="329.91px"/>
+        <div class="card-titulo"><p>${quizzPerguntar.questions[i].answers[2].text}</p></div>
+      </div>
+      <div class="card">
+        <img class="card-image" src="${quizzPerguntar.questions[i].answers[3].image}" height = "175.2px"  width="329.91px"/>
+        <div class="card-titulo"><p>${quizzPerguntar.questions[i].answers[3].text}</p></div>
+      </div>
+    `
+  }
+  for (i = 0; i < quizzPerguntar.questions.length; i++) {
+    document.getElementById(`c${i + 1}`).style.backgroundColor = `${quizzPerguntar.questions[i].color}`;
+  }
+}
 
 /*****************************************
  *         COnfigurações tela 3:          *
@@ -136,7 +169,7 @@ function voltarParaHome(clique){
     console.log(conferencia.value);
     console.log(typeof(conferencia.value));
 
-    
+
 //  ESTÁ FUNCIONANDO PARA A TELA 2, MAS NÃO PARA A TELA 3. AINDA NÃO SEI O PORQUE. NAO COMENTEI ESSA PARTE PORQUE ELA NÃO ESTÁ AFETANDO O RESTANTE
     const localTela2 ="embrulho-quizz-tela2";
     const localTela3 = 'tela3-4';
@@ -164,7 +197,7 @@ function voltarParaHome(clique){
         console.log(tela_anterior);
         let tela_atual = document.querySelector(".tela1");
         tela_atual.classList.toggle("hide");
-      
+
     }
 
     else{ console.log("tem algum lugar errado");}
