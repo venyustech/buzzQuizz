1713bea912d9267046c160183122551f9d8f1645
Gardenia Georgia Barbosa De Siqueira <venyustech@gmail.com>
2021-11-19 21:42:19 -0300

    Adicionando mobile v1.0

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
 /*****************************************
-*         COnfigurações gerais:          *
-******************************************/
+ *         COnfigurações gerais:          *
+ ******************************************/
 
-let quizzesUsuarioArmazenados = 0;//variável global onde será alterado o valor se adicionado quizzes pelo usuário
+let quizzesUsuarioArmazenados = 0; //variável global onde será alterado o valor se adicionado quizzes pelo usuário
 
 /*****************************************
-*         COnfigurações tela 1:          *
-******************************************/
+ *         COnfigurações tela 1:          *
+ ******************************************/
 carregarQuizzes();
 
-
 function carregarQuizzes() {
-    console.log("tá indo aqui");
-    const quizzes = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
-    quizzes.then(exibicaoDeQuizz);
-    quizzes.catch(verificacaoDeErro);
+  console.log("tá indo aqui");
+  const quizzes = axios.get(
+    "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
+  );
+  quizzes.then(exibicaoDeQuizz);
+  quizzes.catch(verificacaoDeErro);
 
-    if(quizzesUsuarioArmazenados==0){
-        let listagem_quizz_usuario = document.querySelector(".quizzes-usuario");
-        listagem_quizz_usuario.style.display= 
-        "none";
-    }
+  if (quizzesUsuarioArmazenados == 0) {
+    let listagem_quizz_usuario = document.querySelector(".quizzes-usuario");
+    listagem_quizz_usuario.style.display = "none";
+  }
 }
 
-function verificacaoDeErro(erro){
-alert("deu ruim");
-console.log(erro);
+function verificacaoDeErro(erro) {
+  alert("deu ruim");
+  console.log(erro);
 }
 
-function exibicaoDeQuizz(resposta){
-    let nomes = [];
-    let capas = [];
-    let imprimir = 0;
-    let listagem_quizz = document.querySelector(".lista_servidor");
+function exibicaoDeQuizz(resposta) {
+  let nomes = [];
+  let capas = [];
+  let imprimir = 0;
+  let listagem_quizz = document.querySelector(".lista_servidor");
 
-    listagem_quizz.innerHTML = "";
+  listagem_quizz.innerHTML = "";
 
-    for(let ii = 0;ii<resposta.data.length;ii++){ 
-    
-    if(nomes.includes(resposta.data[ii].title)){
-        console.log("já tem um quizz com esse nome");
-        //verificar mais uma variável para ver se é mesmo o mesmo quizz
-        imprimir = false;
+  for (let ii = 0; ii < resposta.data.length; ii++) {
+    if (nomes.includes(resposta.data[ii].title)) {
+      console.log("já tem um quizz com esse nome");
+      //verificar mais uma variável para ver se é mesmo o mesmo quizz
+      imprimir = false;
+    } else {
+      nomes.push(resposta.data[ii].title);
+      capas.push(resposta.data[ii].image);
+      console.log("ok, é um quizz novo");
+      imprimir = true;
     }
-         else{ 
-        nomes.push(resposta.data[ii].title);
-        capas.push(resposta.data[ii].image);console.log("ok, é um quizz novo");
-        imprimir = true;}
-        
-if(imprimir){
-        //CONFERIR SE TEM QUIZZES REPETIDOS
-        
-        listagem_quizz.innerHTML+=`<article class = "quizz" onclick = 'buscarQuizzSelecionado(this)'>
+
+    if (imprimir) {
+      //CONFERIR SE TEM QUIZZES REPETIDOS
+
+      listagem_quizz.innerHTML += `<article class = "quizz" onclick = 'buscarQuizzSelecionado(this)'>
       
         <figure>
         <div class = "degrade-escuro"></div>
@@ -59,62 +59,76 @@ if(imprimir){
           <span>${resposta.data[ii].title}</span>
           <span class = "identificacao">${resposta.data[ii].id}</span>
         </figure>
-      </article>`
-    }}
-//fim criacao quizz
-console.log(nomes);
-nomes = [];
-capas = [];
+      </article>`;
+    }
+  }
+  //fim criacao quizz
+  console.log(nomes);
+  nomes = [];
+  capas = [];
 }
 
-function buscarQuizzSelecionado(id){ 
+function buscarQuizzSelecionado(id) {
+  let identificacao = id.querySelector(".identificacao").innerText;
+  let quizz = axios(
+    `https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${identificacao}`
+  );
 
-    let identificacao = id.querySelector(".identificacao").innerText;
-    let quizz = axios(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${identificacao}`);
-
-    quizz.then(criarQuizzSelecionado);
-    quizz.catch(verificacaoDeErro);
-    
+  quizz.then(criarQuizzSelecionado);
+  quizz.catch(verificacaoDeErro);
 }
 
-function criarQuizzSelecionado(resposta){
-    console.log("selecionou um quizz");
-    // se o quizz existir, e ele for selecionado, a tela1 será escondida para abrir a "tela2" com o quizz
-    let tela_anterior = document.querySelector(".tela1");
-    tela_anterior.classList.toggle("hide");
-    console.log(tela_anterior);
-    let tela_atual = document.querySelector(".embrulho-quizz-tela2");
-    tela_atual.classList.toggle("hide");
-    //criar o quiz com os itens da resposta
+function criarQuizzSelecionado(resposta) {
+  console.log("selecionou um quizz");
+  // se o quizz existir, e ele for selecionado, a tela1 será escondida para abrir a "tela2" com o quizz
+  let tela_anterior = document.querySelector(".tela1");
+  tela_anterior.classList.toggle("hide");
+  console.log(tela_anterior);
+  let tela_atual = document.querySelector(".embrulho-quizz-tela2");
+  tela_atual.classList.toggle("hide");
+  //criar o quiz com os itens da resposta
 }
 
-function tela1tela3(resposta){
-    let tela_anterior = document.querySelector(".tela1");
-    tela_anterior.classList.toggle("hide");
-    console.log(tela_anterior);
-    let tela_atual = document.querySelector(".tela3-1");
-    tela_atual.classList.toggle("hide");
-    console.log("ok");
+function tela1tela3(resposta) {
+  let tela_anterior = document.querySelector(".tela1");
+  tela_anterior.classList.toggle("hide");
+  console.log(tela_anterior);
+  let tela_atual = document.querySelector(".tela3-1");
+  tela_atual.classList.toggle("hide");
+  console.log("ok");
 }
-
 
 /*****************************************
-*         COnfigurações tela 2:          *
-******************************************/
+ *         COnfigurações tela 2:          *
+ ******************************************/
 
-document.getElementById("c1").style.backgroundColor="##434CA0";
-document.getElementById("c2").style.backgroundColor="#a0438d";
+document.getElementById("c1").style.backgroundColor = "#434CA0";
+document.getElementById("c2").style.backgroundColor = "#a0438d";
+function abreQuizz() {
+  document.getElementById("titulo1").style.backgroundImage =
+    "url('assets/tela2/Rectangle 42.png')";
+  document.getElementById("pergunta1").style.backgroundColor = "#434CA0";
+  document.getElementById("pergunta2").style.backgroundColor = "red";
 
+  const tituloDaPaginaLoading = document.querySelector(".quizz-titulo");
+  tituloDaPaginaLoading.innerHTML += `<p>O quão PotterHead é você?</p>`;
+
+  let tituloDaPergunta = document.querySelector("#pergunta1");
+  tituloDaPergunta.innerHTML += `<p>Em qual animal Olho-Tonto Moody transfigurou Malfoy?</p>`;
+
+  tituloDaPergunta = document.querySelector("#pergunta2");
+  tituloDaPergunta.innerHTML += `<p>Qual dos objetos abaixo NÃO é uma horcrux?</p>`;
+}
+abreQuizz();
 
 /*****************************************
-*         COnfigurações tela 3:          *
-******************************************/
-
+ *         COnfigurações tela 3:          *
+ ******************************************/
 
 /*****************************************
-*         COnfigurações Navegação Entre Telas      *
-******************************************/
-
+ *         COnfigurações Navegação Entre Telas      *
+ ******************************************/
+/*
 function voltarParaHome(clique){
     console.log("fui chamado para voltar pra tela1");
     let conferencia = clique.parentElement.classList;
@@ -154,4 +168,4 @@ function voltarParaHome(clique){
     }
 
     else{ console.log("tem algum lugar errado");}
-}
+}*/
