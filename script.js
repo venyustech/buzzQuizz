751edd55fb27e6c12c6893c8ed1f6b22dcc79ef4
rafaelarissa <rafaela.larissalj@gmail.com>
2021-11-20 17:53:04 -0300

    função para verificar se perguntas e respostas são válidos

--- script.js
+++ script.js
@@ -139,29 +139,29 @@ function quizzLoading(answer) {
 
   for (let i = 0; i < quizzPerguntar.questions.length; i++) {
     let embaralhador = [];
-let comprimento = quizzPerguntar.questions[i].answers.length;
+    let comprimento = quizzPerguntar.questions[i].answers.length;
 
-while(embaralhador.length<comprimento){
-  let numero = Math.floor(Math.random() * 4);
-  if(!embaralhador.includes(numero)){
-embaralhador.push(numero);
-  }
-}
-tituloDaPaginaLoading.innerHTML += `<div class="pergunta-embrulho">
+    while (embaralhador.length < comprimento) {
+      let numero = Math.floor(Math.random() * 4);
+      if (!embaralhador.includes(numero)) {
+        embaralhador.push(numero);
+      }
+    }
+    tituloDaPaginaLoading.innerHTML += `<div class="pergunta-embrulho">
 <div class="pergunta-caixa">
   <div class="pergunta-titulo" id="c${i + 1}"><p>${quizzPerguntar.questions[i].title}</p>
 </div>
 <div class="cards-embrulho${i}">  </div>
 `
-let ii=0;
-while (ii<comprimento){
-let localRespostas = document.querySelector(`.cards-embrulho${i}`);
-localRespostas.innerHTML+= `<div class="card ${ii}" onclick = "respostaSelecionada(this)">
+    let ii = 0;
+    while (ii < comprimento) {
+      let localRespostas = document.querySelector(`.cards-embrulho${i}`);
+      localRespostas.innerHTML += `<div class="card ${ii}" onclick = "respostaSelecionada(this)">
 <img class="card-image" src="${quizzPerguntar.questions[i].answers[embaralhador[ii]].image}" height = "175.2px"  width="329.91px"/>
 <div class="card-titulo"><p>${quizzPerguntar.questions[i].answers[embaralhador[ii]].text}</p></div>
 </div>`
-ii++
-}
+      ii++
+    }
 
   }
   for (i = 0; i < quizzPerguntar.questions.length; i++) {
@@ -227,21 +227,16 @@ function validarInformacoesBasicas() {
 
   if (tituloQuiz.length < 20 || tituloQuiz.length > 65) {
     alert('Validação falhou, titulo do quiz deve ter no mínimo 20 e no máximo 65 caracters');
-    // document.querySelector("#titulo").value = "";
   }
-  else if (!isValidHttpUrl || URLquiz.length < 2) {
+  else if (!isValidHttpUrl || URLquiz === '') {
     alert('Validação falhou, url deve ter formato válido');
-    // document.querySelector("#urlImagem").value = "";
   }
   else if (parseInt(quantPerguntas) < 3 || parseInt(quantPerguntas) !== parseInt(quantPerguntas)) {
     alert('Validação falhou, o quiz deve ter no mínimo 3 perguntas');
-    // document.querySelector("#numeroPerguntas").value = "";
   }
   else if (parseInt(quantNiveis) < 2 || parseInt(quantNiveis) !== parseInt(quantNiveis)) {
     alert('Validação falhou, o quiz deve ter no mínimo 2 níveis');
-    // document.querySelector("#numeroNiveis").value = "";
   }
-
   else {
     console.log('passei');
     passarProximaFormulario();
@@ -249,6 +244,43 @@ function validarInformacoesBasicas() {
 
   //se passou, essas informacoes tem que ser armazenadas em forma de objeto para depoir criarem o novo quizz. E o botão deve chamar a próxima página
 }
+
+function validarPerguntaseRespostas() {
+  let textoPergunta = document.querySelector("#textoPergunta").value;
+  let corFundo = document.querySelector("#corFundo").value;
+  let textoResposta = document.querySelector("#textoResposta").value;
+  let urlImagemResposta = document.querySelector("#urlImagemResposta").value;
+
+  let isValidHttpUrl = (urlImagemResposta) => {
+    let url;
+
+    try {
+      url = new URL(urlImagemResposta);
+    } catch (_) {
+      return false;
+    }
+
+    return url.protocol === "http:" || url.protocol === "https:";
+  }
+
+  if (textoPergunta.lentgh < 20 || typeof (textoPergunta) === null) {
+    alert('Validação falhou, a pergunta deve ter no mínimo 20 caracteres');
+  }
+  else if (corFundo.match(/^#[a-f0-9]{6}$/i) !== null) {
+    alert('Validação falhou, cor em formato inválido');
+  }
+  else if (textoResposta !== '') {
+    alert('Validação falhou, o texto da resposta não pode estar vazio');
+  }
+  else if (!isValidHttpUrl || urlImagemResposta === '') {
+    alert('Validação falhou, url deve ter formato válido');
+  }
+  else {
+    console.log('passei');
+    passarProximaFormulario();
+  }
+}
+
 
 
 
